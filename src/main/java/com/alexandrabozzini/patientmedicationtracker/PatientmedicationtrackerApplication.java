31fa82a74b4566b16091044bfd61b7feae38670d
Alexandra Bozzini <alexandrabozzini@gmail.com>
2019-07-09 18:40:52 -0400

    initial commit

---
+++ src/main/java/com/alexandrabozzini/patientmedicationtracker/PatientmedicationtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.alexandrabozzini.patientmedicationtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientmedicationtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientmedicationtrackerApplication.class, args);
+	}
+
+}
